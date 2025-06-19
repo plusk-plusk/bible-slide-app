c334dd2bde7d3499b08781223da081e91524ceed
plusk-plusk <nskh2003@naver.com>
2025-06-19 13:18:42 +0900

    Update bible_loader.py

--- bible_loader.py
+++ bible_loader.py
@@ -1,11 +1,8 @@
-# bible_loader.py
 import json
 
 with open("bible.json", "r", encoding="utf-8") as f:
     bible_data = json.load(f)
 
 def get_verse_text(book, chapter, verse):
-    try:
-        return bible_data[book][chapter][str(verse)]
-    except KeyError:
-        return None
+    key = f"{book}{int(chapter)}:{int(verse)}"
+    return bible_data.get(key)
